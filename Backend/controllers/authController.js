37a202a8cad6d7bcc2f6e598ab8b578834ef62c0
VAIBHAV ZADE <126239600+zade-vaibhav@users.noreply.github.com>
2025-05-19 22:51:17 +0530

    adding username to user model and updating registor controller

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -6,16 +6,16 @@ const generateToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
 
 exports.register = async (req, res) => {
-  const { email, password } = req.body;
+  const { username,email, password } = req.body;
   const exists = await User.findOne({ email });
   if (exists) return res.status(400).json({ message: "User already exists" });
 
   const hashed = await bcrypt.hash(password, 10);
-  const user = await User.create({ email, password: hashed });
+  const user = await User.create({ username,email, password: hashed });
 
   res
     .status(201)
-    .json({ _id: user._id, email: user.email, token: generateToken(user._id) });
+    .json({ _id: user._id, email: user.email,username:user.username, token: generateToken(user._id) });
 };
 
 exports.login = async (req, res) => {
